28e0f3d88898c3b6a63218967887611236101462
Kyle Sauri <kylesauri@gmail.com>
2021-10-17 19:01:36 -0700

    few improvements: adds scaling to points, can now pass a worksheet to a canvas, and cleaned up some of the logic around drawing an axis

--- src/lib.rs
+++ src/lib.rs
@@ -13,10 +13,8 @@ use image::{Rgb, RgbImage};
 
 // TODO: add better error messaging
 // TODO: move this to a parsing module
-#[derive(Debug, Clone)]
+#[derive(Debug)]
 pub struct ParseError;
-
-type WSResult<T> = std::result::Result<T, ParseError>;
 
 impl fmt::Display for ParseError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
@@ -44,7 +42,7 @@ impl From<csv::Error> for ParseError {
 
 // TODO: add support for multiple columns
 // TODO: revisit the structure of `data`
-#[derive(Debug, Default)]
+#[derive(Debug, Default, Clone)]
 pub struct WorkSheet {
     data: Vec<(i64, i64)>,
 }
@@ -54,6 +52,15 @@ impl Index<usize> for WorkSheet {
     fn index(&self, idx: usize) -> &Self::Output {
         // TODO: this shouldn't reveal the underlying vec; maybe add a data type wrapper around the internal vectors?
         &self.data[idx]
+    }
+}
+
+impl IntoIterator for WorkSheet {
+    type Item = (i64, i64);
+    type IntoIter = std::vec::IntoIter<Self::Item>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.data.into_iter()
     }
 }
 
@@ -71,25 +78,6 @@ impl<'a> WorkSheetBuilder<'a> {
         }
     }
 
-    fn build_from_csv(&self) -> WSResult<WorkSheet> {
-        if self.csv_data.is_none() {
-            return Err(ParseError {});
-        }
-        // TODO: move CSV package behind an API (facade)
-        // TODO: this should be moved to `csv_data()` - or better yet dispatch to a private `csv_build` method and just match here?
-        let file = File::open(self.csv_data.unwrap())?; // TODO: better handling on this unwrap
-        let mut rdr = csv::Reader::from_reader(file);
-        let mut data: Vec<(i64, i64)> = vec![];
-        for result in rdr.records() {
-            let record = result?;
-            data.push((
-                record.get(0).ok_or_else(|| ParseError)?.parse::<i64>()?,
-                record.get(1).ok_or_else(|| ParseError)?.parse::<i64>()?,
-            ));
-        }
-        Ok(WorkSheet { data })
-    }
-
     pub fn csv_data(&mut self, csv_data: &'a str) -> Self {
         let mut result: WorkSheetBuilder = Default::default();
         result.csv_data = Some(csv_data);
@@ -102,27 +90,61 @@ impl<'a> WorkSheetBuilder<'a> {
         result
     }
 
-    pub fn build(self) -> WSResult<WorkSheet> {
+    pub fn build(self) -> Result<WorkSheet, ParseError> {
         if self.csv_data.is_some() {
-            self.build_from_csv()
+            if self.csv_data.is_none() {
+                return Err(ParseError);
+            }
+            // TODO: move CSV package behind an API (facade)
+            // TODO: this should be moved to `csv_data()` - or better yet dispatch to a private `csv_build` method and just match here?
+            let file = File::open(self.csv_data.unwrap())?; // TODO: better handling on this unwrap
+            let mut rdr = csv::Reader::from_reader(file);
+            let mut data: Vec<(i64, i64)> = vec![];
+            for result in rdr.records() {
+                let record = result?;
+                data.push((
+                    record.get(0).ok_or_else(|| ParseError)?.parse::<i64>()?,
+                    record.get(1).ok_or_else(|| ParseError)?.parse::<i64>()?,
+                ));
+            }
+            Ok(WorkSheet { data })
         } else {
             Ok(WorkSheet { data: vec![] })
         }
     }
 }
 
-#[derive(PartialEq, Eq, Debug, Clone, Copy)]
+#[derive(PartialEq, Eq, Debug, Clone, Copy, PartialOrd, Ord)]
 pub struct Point {
-    x: u32,
-    y: u32,
+    x: i64,
+    y: i64,
 }
 
 impl Point {
-    pub fn transform(self, height: u32) -> Point {
+    pub fn transform(self, height: i64) -> Point {
         Point {
             x: self.x,
             y: height - self.y,
         }
+    }
+
+    pub fn scaled(&self, length: i64) -> Vec<Point> {
+        if length == 0 {
+            return vec![self.clone()];
+        }
+
+        let mut result = vec![];
+        let (start_x, end_x) = (self.x - length, self.x + length);
+        let (start_y, end_y) = (self.y - length, self.y + length);
+        for current_x in start_x..=end_x {
+            for current_y in start_y..=end_y {
+                result.push(Point {
+                    x: current_x,
+                    y: current_y,
+                });
+            }
+        }
+        result
     }
 }
 
@@ -132,16 +154,25 @@ pub trait Drawable {
 
 pub struct Canvas {
     filename: &'static str,
-    border: u32,
+    border: i64, // TODO: fix this usize vs i64 etc madness
     image_buffer: RgbImage,
+    height: i64,
+    worksheet: WorkSheet,
+}
+
+enum Axis {
+    X,
+    Y,
 }
 
 impl Canvas {
-    pub fn new(border: u32, filename: &'static str) -> Self {
+    pub fn new(border: i64, filename: &'static str, height: i64, worksheet: WorkSheet) -> Self {
         Self {
             filename,
             border,
-            image_buffer: RgbImage::new(600, 600),
+            image_buffer: RgbImage::new(height as u32, height as u32),
+            height,
+            worksheet,
         }
     }
 
@@ -155,29 +186,68 @@ impl Canvas {
         }
     }
 
+    fn draw_tick(&mut self, point: Point, axis: Axis) {
+        match axis {
+            Axis::Y => {
+                self.image_buffer
+                    .put_pixel((point.x + 1) as u32, point.y as u32, Rgb([255, 0, 0]));
+                self.image_buffer
+                    .put_pixel((point.x + 2) as u32, point.y as u32, Rgb([255, 0, 0]));
+            }
+            Axis::X => {
+                self.image_buffer
+                    .put_pixel(point.x as u32, (point.y - 1) as u32, Rgb([255, 0, 0]));
+                self.image_buffer
+                    .put_pixel(point.x as u32, (point.y - 2) as u32, Rgb([255, 0, 0]));
+            }
+        }
+    }
+
     fn draw_axis(&mut self) {
-        for y in 0..600 {
+        for y in 0..self.height {
             if y % 10 == 0 {
-                self.image_buffer
-                    .put_pixel(self.border + 1, y, Rgb([255, 0, 0]));
-                self.image_buffer
-                    .put_pixel(self.border + 2, y, Rgb([255, 0, 0]));
+                self.draw_tick(Point { x: self.border, y }, Axis::Y);
             }
             self.image_buffer
-                .put_pixel(self.border, y, Rgb([255, 0, 0]));
+                .put_pixel(self.border as u32, y as u32, Rgb([255, 0, 0]));
         }
 
         // draw bottom axis
-        for x in 0..600 {
+        for x in 0..self.height {
             if x % 10 == 0 {
-                self.image_buffer
-                    .put_pixel(x, 600 - self.border - 1, Rgb([255, 0, 0]));
-                self.image_buffer
-                    .put_pixel(x, 600 - self.border - 2, Rgb([255, 0, 0]));
+                self.draw_tick(
+                    Point {
+                        x: x,
+                        y: self.height - self.border,
+                    },
+                    Axis::X,
+                );
             }
-            self.image_buffer
-                .put_pixel(x, 600 - self.border, Rgb([255, 0, 0]));
+            self.image_buffer.put_pixel(
+                x as u32,
+                (self.height - self.border) as u32,
+                Rgb([255, 0, 0]),
+            );
         }
+    }
+
+    fn draw_point(&mut self, mut point: Point) {
+        point = point.transform(self.height);
+        let scaled_points = point.scaled(2);
+        for scaled_point in scaled_points.iter() {
+            self.image_buffer.put_pixel(
+                (scaled_point.x + self.border) as u32,
+                (scaled_point.y - self.border) as u32,
+                Rgb([200, 0, 0]),
+            );
+        }
+    }
+
+    fn draw_points(&mut self) {
+        self.worksheet
+            .clone()
+            .into_iter()
+            .for_each(|(x, y)| self.draw_point(Point { x, y }));
     }
 }
 
@@ -185,6 +255,7 @@ impl Drawable for Canvas {
     fn draw(&mut self) {
         self.fill_in_white();
         self.draw_axis();
+        self.draw_points();
         self.save();
     }
 }
@@ -212,7 +283,7 @@ mod tests {
 
         match ws {
             Ok(_) => panic!("unexpected"),
-            Err(ParseError {}) => {}
+            Err(ParseError) => {}
         }
     }
 
@@ -234,6 +305,43 @@ mod tests {
         for (idx, &input) in inputs.iter().enumerate() {
             assert_eq!(input.transform(height), expected[idx]);
         }
+    }
+
+    #[test]
+    fn get_scaled_point() {
+        let point = Point { x: 10, y: 20 };
+        let mut expected = vec![
+            Point { x: 8, y: 18 },
+            Point { x: 9, y: 18 },
+            Point { x: 10, y: 18 },
+            Point { x: 11, y: 18 },
+            Point { x: 12, y: 18 },
+            Point { x: 8, y: 19 },
+            Point { x: 9, y: 19 },
+            Point { x: 10, y: 19 },
+            Point { x: 11, y: 19 },
+            Point { x: 12, y: 19 },
+            Point { x: 8, y: 20 },
+            Point { x: 9, y: 20 },
+            Point { x: 10, y: 20 },
+            Point { x: 11, y: 20 },
+            Point { x: 12, y: 20 },
+            Point { x: 8, y: 21 },
+            Point { x: 9, y: 21 },
+            Point { x: 10, y: 21 },
+            Point { x: 11, y: 21 },
+            Point { x: 12, y: 21 },
+            Point { x: 8, y: 22 },
+            Point { x: 9, y: 22 },
+            Point { x: 10, y: 22 },
+            Point { x: 11, y: 22 },
+            Point { x: 12, y: 22 },
+        ];
+        let mut actual = point.scaled(2);
+        expected.sort();
+        actual.sort();
+
+        assert_eq!(actual, expected);
     }
 
     // TODO: add test for vectors
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 extern crate image;
-use graph_buddy::{Canvas, Drawable};
+use graph_buddy::{Canvas, Drawable, WorkSheet, WorkSheetBuilder};
 
 fn main() {
-    let mut canvas = Canvas::new(10, "foo.png");
+    let ws: WorkSheet = WorkSheetBuilder::new()
+        .csv_data("data/worksheet1.csv")
+        .build()
+        .unwrap();
+    let mut canvas = Canvas::new(10, "foo.png", 600, ws);
     canvas.draw();
 }
